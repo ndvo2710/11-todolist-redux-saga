b3344e89dcb058f65acb18c903682a712b5e3ebf
Kevin Nguyen Vo <nguyen.vo@oculus.com>
2021-06-14 00:59:41 -0700

    Header

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
-
-import './App.css';
+import { BrowserRouter } from "react-router-dom";
+import "./App.css";
+import Header from "./components/Home/Header/Header";
 
 function App() {
-  return (
-    <div className="App">
-      Hello World
-    </div>
-  );
+  return <div className="App">
+      <BrowserRouter>
+        <Header />
+      </BrowserRouter>
+  </div>;
 }
 
 export default App;
