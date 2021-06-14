4da55b8bf25e6f3fd782261249f4365c8cc7b9df
Kevin Nguyen Vo <nguyen.vo@oculus.com>
2021-06-14 01:03:06 -0700

    Route Setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import "./App.css";
 import Header from "./components/Home/Header/Header";
+import About from "./pages/About/About";
+import Contact from "./pages/Contact/Contact";
+import Home from "./pages/Home/Home";
+import TodoListSaga from "./pages/TodoListSaga/TodoListSaga";
 
 function App() {
   return <div className="App">
       <BrowserRouter>
         <Header />
+        <Switch>
+            <Route exact path='/home' component={Home} />
+            <Route exact path='/about' component={About} />
+            <Route exact path='/contact' component={Contact} />
+            <Route exact path='/todolistsaga' component={TodoListSaga} />
+        </Switch>
       </BrowserRouter>
   </div>;
 }
